be39c3fd308e8de96536a0e9a5c74f0c0233fdb6
r-elopre <donvgdong052@gmail.com>
2024-02-18 10:29:29 +0800

    Use navigation to find a way to get CategoryDetailScreen.

--- app/src/main/java/com/example/myrecipeapp/RecipeScreen.kt
+++ app/src/main/java/com/example/myrecipeapp/RecipeScreen.kt
@@ -1,6 +1,7 @@
 package com.example.myrecipeapp
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
@@ -24,7 +25,8 @@ import coil.compose.rememberAsyncImagePainter
 
 
 @Composable
-fun RecipeScreen(modifier: Modifier = Modifier){
+fun RecipeScreen(modifier: Modifier = Modifier,
+                 navigateToDetail: (Category) ->Unit){
     val recipeViewModel:MainViewModel = viewModel()
     val viewSate by recipeViewModel.categoriesState
 
@@ -37,18 +39,20 @@ fun RecipeScreen(modifier: Modifier = Modifier){
                 Text("ERROR OCCURRED")
             }
             else ->{
-                CategoryScreen(categories = viewSate.list )
+                CategoryScreen(categories = viewSate.list,
+                    navigateToDetail)
             }
         }
     }
 }
 
 @Composable
-fun CategoryScreen(categories: List<Category>){
+fun CategoryScreen(categories: List<Category>,
+                   navigateToDetail: (Category) ->Unit){
     LazyVerticalGrid(GridCells.Fixed(2), modifier = Modifier.fillMaxSize()){
         items(categories){
             category ->
-            CategoryItem(category = category)
+            CategoryItem(category = category, navigateToDetail)
         }
 
     }
@@ -58,10 +62,12 @@ fun CategoryScreen(categories: List<Category>){
 // another composable to make how thing looks like
 
 @Composable
-fun CategoryItem(category: Category){
+fun CategoryItem(category: Category,
+                 navigateToDetail: (Category) ->Unit){
     Column(modifier = Modifier
         .padding(8.dp)
-        .fillMaxSize(),
+        .fillMaxSize()
+        .clickable { navigateToDetail(category) },
         horizontalAlignment = Alignment.CenterHorizontally) {
         Image(
             painter = rememberAsyncImagePainter(category.strCategoryThumb),
